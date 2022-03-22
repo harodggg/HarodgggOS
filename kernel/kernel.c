af4b8347fa4def03ad602ca8e4a9e1b85b7aa214
harodggg <haroldtiansheng@gmail.com>
2022-03-22 20:34:11 +0000

    harold

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -3,9 +3,14 @@ void main() {
 	// video memory (i.e. the top-left of the screen)
 	
 	char* video_memory = (char *) 0xb8000;
+	video_memory = video_memory + 32;
 
 	// At the address pointed to by video_memory, store the character 'X'
 	// (i.e. display 'X' in the top-left of the screen) . 
 	
 	*video_memory = 'x';
+	do_something();
+}
+ 
+int do_something() { 
 }
