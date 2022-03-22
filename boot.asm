ba99729b5b7acfe582f77983dc3ed3f87e3ba91f
harodggg <haroldtiansheng@gmail.com>
2022-03-23 03:44:39 +0800

    harold

--- boot.asm
+++ boot.asm
@@ -1,33 +1,61 @@
-; A boot sector that enters 32-bit protected mode.
+; A boot sector that boots a C kernel in 32-bit protected mode 
 [org 0x7c00]
+KERNEL_OFFSET equ 0x1000	; This is the memory offset ot which we will load our kernel
+
+	mov [BOOT_DRIVE], dl 	; BIOS stores our boot drive in DL, so it's
+				; best to remember this for later.
+
+	mov bp, 0x9000		; Set-up the stack.
+	mov sp, bp
 	
-	mov bp, 0x9000				; Set the stack.
-	mov sp, bp				
+	mov bx, MSG_REAL_MODE	; Anounce that we are starting
+	call print_string	; booting from 16-bit real mode 
 
-	mov bx, MSG_REAL_MODE
-	call print_string		
+	call load_kernel	; Load our kernel
 
-	call switch_to_pm 			; Note that we never return from here. 
+	call switch_to_pm 	; Switch to protected mode. from which 
+				; we will not return 
 
 	jmp $
 
-%include "print_string.asm"		
-%include "gdt.asm"
-%include "print_string_pm.asm"
-%include "switch_to_pm.asm"
+	; Include our useful, hard-earned routines 
 
-[bits 32]
-; This is where we arrive after switching to and initialising protected mode.
-BEGIN_PM:
-
-	mov ebx, MSG_PROT_MODE
-	call print_string_pm 			; Use our 32-bit print routine . 
+	%include "print_string.asm"
+	%include "disk_load.asm"
+	%include "gdt.asm"
+	%include "print_string_pm.asm"
+	%include "switch_to_pm.asm"
 	
-	jmp $					; Hang. 
+	[bits 16]
+
+	; load_kernel
+	load_kernel:
+		mov bx, MSG_LOAD_KERNEL		; Print a message to say we loading the kernl 
+		call print_string
+		
+		mov bx, KERNEL_OFFSET		; Set-up parameters for our disk_load routine, so 
+		mov dh, 15			; that we load the first 15 sectors (excluding 
+		mov dl, [BOOT_DRIVE]		; the boot sector) from the boot disk (i.e. our 
+		call disk_load 			; kernel code) to address KENEL_OFFSET
+
+		ret 
+
+	[bits 32]
+	BEGIN_PM:
+		mov ebx, MSG_PROT_MODE		; Use our 320bit print routine to 
+		call print_string_pm 		; announce we are in protected mode 
+		
+		call KERNEL_OFFSET 		; Now jump to the address of our loaded 
+						; kernel code , assume the brace position,
+						; and cross your fingers. Here we go!
+
+		jmp $ 				; Hang. 
 
 ; Global variables 
-MSG_REAL_MODE db "Started in 16-bit Real Mode", 0
-MSG_PROT_MODE db "Successfully landed in 32-bit Protected Mode", 0
+BOOT_DRIVE		db 0
+MSG_REAL_MODE 		db "Started in 16-bit Real Mode", 0
+MSG_PROT_MODE 		db "Successfully landed in 32-bit Protected Mode", 0
+MSG_LOAD_KERNEL 	db "Loading kernel into memory.", 0
 
 ; Bootsector padding 
 times 510-($-$$) db 0 
